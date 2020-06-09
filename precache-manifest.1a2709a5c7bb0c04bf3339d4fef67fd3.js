6c672a57b3dc1fceb323d62d2ea17799bc692db3
Dawson Hensel <dawsonhensel@gmail.com>
2020-06-08 21:55:49 -0700

    moved npm build to root of repo

---
+++ precache-manifest.1a2709a5c7bb0c04bf3339d4fef67fd3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e45ebaa0f62d455285d99f9341986985",
+    "url": "/static/media/no_image_placeholder.e45ebaa0.jpg"
+  },
+  {
+    "revision": "1411126101669f8c05c86ade6b5673eb",
+    "url": "/static/media/backgroundDark.14111261.jpg"
+  },
+  {
+    "revision": "26e9e6f9242bef579cfb8316263c8c11",
+    "url": "/static/media/background.26e9e6f9.jpg"
+  },
+  {
+    "revision": "acfa8fa7d1879b00a5b28af8eb4d1b7b",
+    "url": "/static/media/GoogleBooksSearch.acfa8fa7.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca4af81dc467e3e308db",
+    "url": "/static/js/main.09fcd45c.chunk.js"
+  },
+  {
+    "revision": "1196836451bb87118f06",
+    "url": "/static/js/2.8d908e83.chunk.js"
+  },
+  {
+    "revision": "ca4af81dc467e3e308db",
+    "url": "/static/css/main.752d7edb.chunk.css"
+  },
+  {
+    "revision": "e93b05cc811cf3a1f871c74d358d9481",
+    "url": "/index.html"
+  }
+];
